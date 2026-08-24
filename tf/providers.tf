0b9553486ca09f45158053fe4954673fc670b26b
Lars Kellogg-Stedman <lars@redhat.com>
2026-08-24 08:19:16 -0400

    Use exec-based auth for kubernetes/helm providers
    The data.aws_eks_cluster_auth token is cached in state and not
    re-evaluated when the cluster resource has pending changes, causing
    stale token authentication failures on subsequent plans.

--- tf/providers.tf
+++ tf/providers.tf
@@ -37,20 +37,24 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = aws_eks_cluster.cluster.name
-}
-
 provider "kubernetes" {
   host                   = aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.cluster.name]
+  }
 }
 
 provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.cluster.name]
+    }
   }
 }
